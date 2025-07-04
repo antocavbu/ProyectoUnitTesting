b181f524d5fd80efac07eee52a38addc7c621941
unknown <antobustosc@gmail.com>
2025-07-03 22:37:49 -0400

    green GET con respuesta 200

--- src/app.js
+++ src/app.js
@@ -3,11 +3,18 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-// POST /courses → 201 con id y title
+const courses = [];
 let nextCourseId = 1;
+
+// POST /courses → 201 con id y title
 app.post('/courses', (req, res) => {
   const course = { id: nextCourseId++, ...req.body };
   res.status(201).json(course);
+});
+
+// GET /courses → 200 + array
+app.get('/courses', (req, res) => {
+  res.status(200).json(courses);
 });
 
 // catch all para el resto de las rutas 
