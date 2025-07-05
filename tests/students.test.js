1aede205a1380b32a791db04f5c50d95bb1b5821
unknown <antobustosc@gmail.com>
2025-07-05 18:47:11 -0400

    test red Get students

--- tests/students.test.js
+++ tests/students.test.js
@@ -2,6 +2,7 @@ import request from 'supertest';
 import app     from '../src/app.js';
 
 describe('Student CRUD (Red → Green)', () => {
+  // test para crear un estudiante
   it('POST /students → debería devolver 201 y body.id, name, email (Red)', () =>
     request(app)
       .post('/students')
@@ -11,6 +12,18 @@ describe('Student CRUD (Red → Green)', () => {
         if (!res.body.id) throw new Error('Falta id');
         if (res.body.name !== 'Alice') throw new Error('Name incorrecto');
         if (res.body.email !== 'alice@example.com') throw new Error('Email incorrecto');
+      })
+  );
+  
+  // test para Get Students
+  it('GET /students → debería devolver 200 y un array', () =>
+    request(app)
+      .get('/students')
+      .expect(200)
+      .expect(res => {
+        if (!Array.isArray(res.body)) {
+          throw new Error('Se esperaba un array');
+        }
       })
   );
 });
